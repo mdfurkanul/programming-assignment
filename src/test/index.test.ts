fb5fa586945134c212493d554dec9d1fde1f0bea
MD Furkanul Islam <mdfish191@studentmail.oru.se>
2025-01-14 14:21:52 +0100

    add more test case

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,5 +1,11 @@
 import { describe, it, expect } from "vitest";
-import { commandsValidation, rotationLeftRight, moveForward } from "..";
+import {
+  commandsValidation,
+  rotationLeftRight,
+  moveForward,
+  commandExecute,
+} from "..";
+import testCases from "../input/input_test";
 
 describe("commandsValidation", () => {
   it("shoud return true for the inputs LFFRRLL", () => {
@@ -63,5 +69,49 @@ describe("moveForward", () => {
     expect(
       moveForward({ wide: 2, deep: 2, orientation: "W", roomSize: [5, 5] })
     ).toStrictEqual([1, 2]);
+  });
+});
+
+describe("commandExecute", () => {
+  it("should return 1 3 N for args 5 5; 1 2 N ; RFRFFRFRF", () => {
+    expect(commandExecute(testCases[0])).toStrictEqual({
+      wide: 1,
+      deep: 3,
+      orientation: "N",
+    });
+  });
+  it("should return 3 1 E for args 5 5; 0 0 E ; RFLFFLRF", () => {
+    expect(commandExecute(testCases[1])).toStrictEqual({
+      wide: 3,
+      deep: 1,
+      orientation: "E",
+    });
+  });
+  it("should return 4 4 E for args 7 5; 0 0 E ; rFLFFLRFrfffLf", () => {
+    expect(commandExecute(testCases[2])).toStrictEqual({
+      wide: 4,
+      deep: 4,
+      orientation: "E",
+    });
+  });
+
+  it("should return error, Invalid commands", () => {
+    expect(() =>
+      commandExecute({
+        roomSize: [7, 5],
+        positionWithOrientation: [0, 0, "E"],
+        commands: "RK",
+      })
+    ).toThrow("Invalid commands");
+  });
+
+  it("should return error, out of boundry", () => {
+    expect(() =>
+      commandExecute({
+        roomSize: [3, 5],
+        positionWithOrientation: [0, 0, "E"],
+        commands: "FfFLFRFl",
+      })
+    ).toThrow("Robot out of boundary");
   });
 });
